460ccec304b7ef3889e7e7b2c505ae822d92d8f4
Stepan Fedorov <fedorovstepan@gmail.com>
2013-03-10 02:07:43 +0400

    Добавил вьюхи и запись в shared preferences

--- MoneyLog/src/com/example/moneylog/AuthActivity.java
+++ MoneyLog/src/com/example/moneylog/AuthActivity.java
@@ -1,15 +1,129 @@
 package com.example.moneylog;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Activity with the login, password fields
+ */
 public class AuthActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.auth);
+
+        Map<String, String> loginAncPassword = getLoginAndPass();
+        if (checkAuth(loginAncPassword.get(getString(R.string.login_key)), loginAncPassword.get(getString(R.string.password_key)))) {
+            redirectToMainActivity();
+        }
+    }
+
+    /**
+     * Enter button listener
+     *
+     * @param view enter button reference
+     */
+    public void enterButtonListener(View view) {
+
+        String login = ((EditText) findViewById(R.id.login)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+
+        if (!checkAuth(login, password)) {
+            return; // TODO wrong cred message
+        }
+
+        if (checkRememberMe()) {
+            savePasswordAndLoginToSettings(login, password);
+        } else {
+            clearPasswordAndLoginToSettings();
+        }
+
+        redirectToMainActivity();
+    }
+
+    private void redirectToMainActivity() {
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Check auth params. Now it is fake check.
+     * TODO normal rights check
+     *
+     * @param login
+     * @param password
+     * @return
+     */
+    private boolean checkAuth(String login, String password) {
+        return "1".equals(login) && "1".equals(password);
+    }
+
+    /**
+     * Check if Remember me check box checked
+     *
+     * @return
+     */
+    private boolean checkRememberMe() {
+        CheckBox checkBox = (CheckBox) findViewById(R.id.remember_me_check_box);
+        return checkBox.isChecked();
+    }
+
+    /**
+     * Save login and password to shared preferences
+     *
+     * @param login
+     * @param password
+     */
+    private void savePasswordAndLoginToSettings(String login, String password) {
+
+        saveToSharedPref(true, login, password);
+    }
+
+    /**
+     * Clear login and password from shared preferences
+     */
+    private void clearPasswordAndLoginToSettings() {
+        saveToSharedPref(false, "", "");
+    }
+
+    private void saveToSharedPref(boolean isRememberMeChecked, String login, String password) {
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.shared_prop_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(getString(R.string.remember_me_key), isRememberMeChecked);
+        editor.putString(getString(R.string.login_key), login);
+        editor.putString(getString(R.string.password_key), password);
+    }
+
+
+    /**
+     * Get login and password map from shared file
+     *
+     * @return
+     */
+    public Map<String, String> getLoginAndPass() {
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.shared_prop_key), Context.MODE_PRIVATE);
+        boolean isRemember = sharedPref.getBoolean(getString(R.string.remember_me_key), false);
+        Map<String, String> loginAndPass = Collections.singletonMap("","");
+        if(isRemember){
+            loginAndPass.put(getString(R.string.login_key), sharedPref.getString(getString(R.string.login_key),""));
+            loginAndPass.put(getString(R.string.password_key), sharedPref.getString(getString(R.string.password_key),""));
+        }
+
+        return loginAndPass;
     }
 }
---
+++ MoneyLog/src/com/example/moneylog/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.moneylog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Main activity. It is an enter point to all functions of application
+ */
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
